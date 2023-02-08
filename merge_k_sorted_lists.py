e1218ef4739eb8bf431ce9dd61e3a2fd3db8595b
nishant hegde <nishant.arun.hegde@gmail.com>
2023-02-08 06:12:47 -0800

    new commits for practice probelm

---
+++ merge_k_sorted_lists.py
@@ -0,0 +1,13 @@
+"""Practice leet code problem"""
+
+
+def merge_k_sorted():
+    pass
+
+
+def test_merge():
+    print("thank you for everything I have received")
+
+
+if __name__ == "__main__":
+    test_merge()
